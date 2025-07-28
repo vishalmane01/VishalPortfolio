946c3212f7b9a6cab8a4e7fb7a9fe546356d0c27
Vishal <vishal012mane@gmail.com>
2025-07-28 22:22:21 +0530

    fixing glow card error

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 
 const GlowCard = ({ children, identifier }) => {
   useEffect(() => {
+    if (typeof window === "undefined") return; // Prevent SSR errors
+
     const handleReady = () => {
       const CONTAINER = document.querySelector(`.glow-container-${identifier}`);
       const CARDS = document.querySelectorAll(`.glow-card-${identifier}`);
